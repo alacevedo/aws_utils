dd94dc80d8bd586194f99c57f8fad218cbd3e1d1
Al <allan.acevedo@gmail.com>
2019-04-04 14:37:36 -0300

    Actualzacion
    Se filtra por StopAtNight en true y se actualiza código para que despliegue las excepciones

--- start_nonprod_instance.py
+++ start_nonprod_instance.py
@@ -1,22 +1,36 @@
+import json
 import boto3
+from botocore.exceptions import ClientError, ParamValidationError
 
 def lambda_handler(event, context):
-    _cli_ec2 = boto3.client('ec2', region_name='us-west-2')
-    _reservations =  _cli_ec2.describe_instances(
+    _cli = boto3.client('ec2', region_name='us-east-2')
+    _reservations =  _cli.describe_instances(
         Filters=[
-            {'Name': 'tag-value', 'Values' : ['non-prod'] }
+            {'Name': 'tag:StopAtNight', 'Values' : ['true'] }
         ]
     )
+    print _reservations
+    print '\n-----------'
 
     _instances = []
     for r in _reservations.get('Reservations'):
         for i in r.get('Instances'):
             _instances.append(i.get('InstanceId'))
-
+    print _instances
+    print '\n-----------'
     try:
-        _start_response = _cli_ec2.start_instances(
-            InstanceIds = _instances
+        _start_response = _cli.start_instances(
+            InstanceIds = _instances,
+            DryRun = True
         )
         print _start_response
-    except:
-        print '[ERROR] : start_instances() failed'
+        return {
+            'statusCode': 200,
+            'body': json.dumps(_start_response)    
+        }
+    except ClientError as e: 
+        _error = '[ERROR] : stop instances failed'
+        return {
+            'statusCode': 500,
+            'body': json.dumps(e)    
+}
