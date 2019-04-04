606cfa8970fd9fdac3c71b3075cfb3d7f4bccc20
Al <allan.acevedo@gmail.com>
2019-04-04 14:32:42 -0300

    Actualizacion
    Se pregunta por le tag StopAtNight igual a true y se actualiza el return

--- stop_nonprod_instance.py
+++ stop_nonprod_instance.py
@@ -1,24 +1,37 @@
+import json
 import boto3
+from botocore.exceptions import ClientError, ParamValidationError
 
 def lambda_handler(event, context):
-    _cli = boto3.client('ec2', region_name='us-west-2')
+    _cli = boto3.client('ec2', region_name='us-east-2')
     _reservations =  _cli.describe_instances(
         Filters=[
-            {'Name': 'tag-value', 'Values' : ['non-prod'] }
+            {'Name': 'tag:StopAtNight', 'Values' : ['true'] }
         ]
     )
+    print _reservations
+    print '\n-----------'
 
     _instances = []
     for r in _reservations.get('Reservations'):
         for i in r.get('Instances'):
             _instances.append(i.get('InstanceId'))
-
+    print _instances
+    print '\n-----------'
     try:
         _stop_response = _cli.stop_instances(
-            DryRun = False
-            , InstanceIds = _instances
-            , Force = False
+            InstanceIds = _instances,
+            DryRun = False,
+            Force = False
         )
         print _stop_response
-    except:
-        print '[ERROR] : stop_instances failed'
+        return {
+            'statusCode': 200,
+            'body': json.dumps(_stop_response)    
+        }
+    except ClientError as e: 
+        _error = '[ERROR] : stop instances failed'
+        return {
+            'statusCode': 500,
+            'body': json.dumps(e)    
+        }
