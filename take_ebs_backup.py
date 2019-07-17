628315c5a792ad22bf605de091185b1962498d12
Al <allan.acevedo@gmail.com>
2019-07-17 12:48:14 -0400

    Create ebs_backup
    Lambda function to take a snapshot based on the tag monthlyBackup = true

---
+++ take_ebs_backup.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+from botocore.exceptions import ClientError, ParamValidationError
+
+def lambda_handler(event, context):
+    _cli = boto3.client('ec2')
+    _volumes =  _cli.describe_volumes(
+            Filters=[{
+                'Name': 'tag:monthlyBackup',
+                'Values' : ['true']
+                }])
+    
+    print(_volumes)
+    print ('\n-----------')
+    
+    _volumesDict = []
+    
+    for i in _volumes.get('Volumes'):
+        
+        _volumesDict.append(i.get('VolumeId'))
+        print(i.get('VolumeId'))
+        
+        try:
+            _response = _cli.create_snapshot(
+            VolumeId = i.get('VolumeId'),
+            DryRun = False
+            )
+            _info = '[INFO] : creating snapshot for ' + i.get('VolumeId')
+            print(_info)
+            
+        except ClientError as e:
+            _error = '[ERROR] : creating snapshot failed for ' + i.get('VolumeId')
+            print(_error)
+            return {
+                    'statusCode': 500,
+                    'body': 'failed to create snapshots'
+                    }
+                    
+    return {
+        'statusCode': 200,
+        'snapshots_taken_on': _volumesDict
+        }
